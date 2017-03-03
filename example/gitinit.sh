a330aef0c1014b9900864f8dd7cfe82a74766a16
jk <jnkiepert@gmail.com>
2017-03-03 15:34:04 +0100

    provide test scritps

---
+++ example/gitinit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+git init
+
+# create basic repository files
+touch .gitignore
+touch cm.txt
+
+# write standard info to files
+printf "bin/\r\n" >> .gitignore
+printf "cm.txt\r\n" >> .gitignore
+printf "*.sh\r\n" >> .gitignore
+
+printf "Initialize empty repository\r\n" >> cm.txt
+printf "\r\nAdd gitignore" >> cm.txt
